0795872aaae8590c86a05fbe17612d7e198017b3
Eemeli Mark <43149988+TheJeme@users.noreply.github.com>
2025-09-30 13:15:20 +0300

    s

--- .config/windows-terminal/profile.ps1
+++ .config/windows-terminal/profile.ps1
@@ -1,7 +1,6 @@
 # Starship prompt
 Invoke-Expression (& starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView -BellStyle None
 if (Get-Command eza -ea SilentlyContinue) { Set-Alias ls eza }
 if (Get-Command fd  -ea SilentlyContinue) { Set-Alias find fd }
 if (Get-Command rg  -ea SilentlyContinue) { Set-Alias grep rg }
